73c4c1cc6f55dce7e18657fae6bf81a708b1b85e
Rosales <RosalesA@trimet.org>
2024-04-09 13:21:54 -0700

    Create algorithm that multiplies size based on app path

--- esm/components/markers/ModeRectangles/index.js
+++ esm/components/markers/ModeRectangles/index.js
@@ -6,7 +6,8 @@ import * as utils from "../../../utils";
  */
 
 function getRectangleSize(zoom) {
-  console.log('getRectangleSize here for duty', window.location);
+  console.log("getRectangleSize here for duty", window.location);
+
   var zoomToCorrectedWidth = {
     9: 2.7,
     10: 3.59,
@@ -19,20 +20,48 @@ function getRectangleSize(zoom) {
     17: 40,
     18: 67.38,
     19: 89.84,
-    20: 116.79
+    20: 116.79,
   };
+
+  var specialPaths = ["/route/", "/vehicle/", "/planner-trip", "/stop/"];
+  var pathname = window.location.pathname;
+  var isSpecialPath = specialPaths.some((path) => pathname.includes(path));
+  console.log('isSpecialPath...?', isSpecialPath);
   var spriteWidth = zoomToCorrectedWidth[zoom] || zoom;
+
+  if (isSpecialPath) {
+    spriteWidth *= 20; // Increase spriteWidth by 20 times for special paths
+  }
+
   return [spriteWidth, spriteWidth]; // Assuming square icons for simplicity
 }
 
-export var LightRailVehicleRectangle = utils.makeRotatedMarker(utils.makeBasicVehicleShape(StyledRectangle.LgShape, StyledRectangle.LgTrackedShape), getRectangleSize);
-export var BusRectangle = utils.makeRotatedMarker(utils.makeBasicVehicleShape(StyledRectangle.Shape, StyledRectangle.TrackedShape), getRectangleSize);
+export var LightRailVehicleRectangle = utils.makeRotatedMarker(
+  utils.makeBasicVehicleShape(
+    StyledRectangle.LgShape,
+    StyledRectangle.LgTrackedShape
+  ),
+  getRectangleSize
+);
+export var BusRectangle = utils.makeRotatedMarker(
+  utils.makeBasicVehicleShape(
+    StyledRectangle.Shape,
+    StyledRectangle.TrackedShape
+  ),
+  getRectangleSize
+);
 export var DetailedRectangle = utils.makeRotatedMarker(function (_ref) {
   var color = _ref.color,
-      highlightColor = _ref.highlightColor,
-      isTracked = _ref.isTracked,
-      routeType = _ref.routeType;
-  return utils.makeVehicleIcon(StyledRectangle, routeType, color, highlightColor, isTracked);
+    highlightColor = _ref.highlightColor,
+    isTracked = _ref.isTracked,
+    routeType = _ref.routeType;
+  return utils.makeVehicleIcon(
+    StyledRectangle,
+    routeType,
+    color,
+    highlightColor,
+    isTracked
+  );
 }, getRectangleSize); // Rename styled components for export
 
 export { StyledRectangle as Styled };
